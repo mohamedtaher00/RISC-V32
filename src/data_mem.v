7c572f3ff9652b3f4464c258375b218685bfd032
Mohammed Taher <motaher20004@gmail.com>
2026-02-23 17:12:43 +0200

    Modify the whole data_mem.v to infer megafunction(use embedded memory)

--- src/data_mem.v
+++ src/data_mem.v
@@ -1,27 +1,55 @@
-module data_mem 
+module data_mem
 	#(parameter MEMORY_SIZE = 12288, parameter ADDR_WIDTH = $clog2(MEMORY_SIZE)
 	)
 	(
 		input [ADDR_WIDTH-1:0] addr,
 	        input we,
 		input re,
-		input [31:0] w_data_MEM, 	
+		input [31:0] w_data_MEM, // data_in
 		input clk,
+		
 
-		output reg [31:0] data 
+
+		// Embedded memory configuration signals
+//		input [13:0] byte_addr, 
+                input [3:0]  byteena,
+
+		output reg [31:0] data // data_out  
 	); 
-	initial begin 
-		$readmemb("data_mem_table.mem", mem) ;
-	end 
-	localparam DEPTH = 2**ADDR_WIDTH ;
-	(* ramstyle = "M9K" *) reg [31:0] mem [0:DEPTH-1] ;
-	//reg [WIDTH-1:0] mem [0:DEPTH-1] ;
-	//write logic - sync write 
-	always @(posedge clk) begin 
-		if (we) 
-			mem[addr] <= w_data_MEM;
-		else // either memory read or will'not deal with memory (write back to the reg file), read enable is insignificant here 
-			data <= mem[addr] ;	
-	end 
 
-endmodule 	
+      
+	wire [11:0] word_addr = addr[13:2];
+
+	altsyncram altsyncram_component (
+       		 .clock0         (clk),
+       		 .address_a      (word_addr),
+       		 .data_a         (w_data_MEM),
+       		 .wren_a         (we),
+       		 .byteena_a      (byteena),
+       		 .q_a            (data)
+
+        // unused
+       // .aclr0          (1'b0),
+       // .aclr1          (1'b0),
+       // .addressstall_a (1'b0),
+       // .clocken0       (1'b1),
+       // .clocken1       (1'b1)
+    );
+
+
+    defparam
+        altsyncram_component.operation_mode                = "SINGLE_PORT",
+        altsyncram_component.width_a                       = 32,
+        altsyncram_component.widthad_a                     = 12,
+        altsyncram_component.numwords_a                    = 3072 , // 12288 bytes / 4 = 3072 
+        altsyncram_component.width_byteena_a               = 4,
+        altsyncram_component.byte_size                     = 8,
+        altsyncram_component.outdata_reg_a                 = "CLOCK0",
+        altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ", // don't care really (it'll never gonna happen) 
+        altsyncram_component.intended_device_family        = "Cyclone V",
+        altsyncram_component.ram_block_type                = "AUTO";
+
+
+
+
+endmodule
