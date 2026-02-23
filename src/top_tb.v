316434e813e456c77cafaa86616ea3d0186ef1de
Mohammed Taher <motaher20004@gmail.com>
2026-02-23 20:13:11 +0200

    Modify top_tb.v ; Add rx, tx signals, remove pc signals

--- src/top_tb.v
+++ src/top_tb.v
@@ -31,22 +31,30 @@ module top_tb ;
         wire reg_write_ctrl_test_tb		;
         wire mem2reg_ctrl_test_tb 		;
 
+
+	wire tx_tb ; 
+	reg rx_tb ; 
+
 	top DUT (
-	.clk(clk_tb), 
-	.reset_n(reset_n_tb), 
+	.clk			(clk_tb), 
+	.reset_n		(reset_n_tb),
+
 	.instruction_test	( instruction_test_tb), 	
 	.write_back_data_test	( write_back_data_test_tb), 	
 	.pc_addr_test 		(pc_addr_test_tb), 	
-	.alu_ctrl_lines_test	(alu_ctrl_lines_test_tb), 	
+	.alu_ctrl_lines_test	(alu_ctrl_lines_test_tb), 
+
+
 	.alu_op_test		(alu_op_test_tb), 	
 	.alu_src_test     	(alu_src_test_tb     	), 	
 	.branch_test      	(branch_test_tb      	), 	
 	.mem_write_ctrl_test  	(mem_write_ctrl_test_tb 	), 	
 	.reg_write_ctrl_test	(reg_write_ctrl_test_tb	), 	
 	.mem2reg_ctrl_test 	(mem2reg_ctrl_test_tb 	),
-	.prog_counter_addr   (prog_counter_addr_tb      ),
-	.prog_counter_next_addr(prog_counter_next_addr_tb  ),
-	.prog_counter_64_bit_addr(prog_counter_64_bit_addr_tb )
+
+	.rx			(rx_tb), 
+	.tx			(tx_tb)
+	
 	); 
 
   always #5 clk_tb = ~clk_tb ;
