402ecb13809161687c4a3239160503519fc27ca4
Mohammed Taher <motaher20004@gmail.com>
2026-02-23 20:02:59 +0200

    Modify the whole instruction_mem.v to infer megafunction(use embedded memory) and Move the stall logic outside to instr_mem_wrapper.v

--- src/instruction_mem.v
+++ src/instruction_mem.v
@@ -2,46 +2,65 @@ module instruction_mem
 	#(parameter INST_MEMORY_SIZE = 16384, parameter ADDR_WIDTH = $clog2(INST_MEMORY_SIZE)
 	)
 	(
+		// Port A  - fetch (read)
 		input clk, 
-		input [ADDR_WIDTH-1:0] read_addr,
-	        input stall,	
+		input [ADDR_WIDTH-1:0] read_addr, // pc
+		
+		output [31:0] readed_data,  // instruction  
+	        
+		input stall,
+			
+    		// Port B — programming port (write only)
 		input [ADDR_WIDTH-1:0] write_addr, 
 		input [31:0] write_data,
-	        input w_en, 
-		output reg [31:0] readed_data 
+	        input w_en 
+
 	); 
 	
-	initial begin 
-		$readmemh("table.mem", mem) ;
-	end 	
-	localparam DEPTH = 2**ADDR_WIDTH ;
-//	reg [31:0] mem [0:DEPTH-1] ;
-	// read logic
-	(* ramstyle = "M9K" *) reg [7:0] mem [0:DEPTH-1] ;
-	always @(posedge clk) begin 
-	  if (!stall) begin 	
-		readed_data[31:24] <= mem[read_addr+3] ;
-		readed_data[23:16] <= mem[read_addr+2] ;
-		readed_data[15:8]  <= mem[read_addr+1] ; 
-		readed_data[7:0]   <= mem[read_addr] ;
-	  end 
-	  else begin 
- 	        readed_data[31:24] <= mem[read_addr-1] ;
-		readed_data[23:16] <= mem[read_addr-2] ;
-		readed_data[15:8]  <= mem[read_addr-3] ; 
-		readed_data[7:0]   <= mem[read_addr-4] ;
+
+    wire [11:0] word_addr_b = read_addr[13:2];
+    wire [11:0] word_addr_a = write_addr[13:2];
+
+
+    altsyncram altsyncram_component (
 	
-	end 
-	end 
+        // Port A — writing (programming) 
+        .clock0         (clk),
+	.clock1		(clk), 
+        .address_a      (word_addr_a),
+        .data_a 	(write_data),
+        .wren_a         (w_en),
 
-	//write logic
-	always @(posedge clk) begin 
-		if (w_en) begin // little endian 
-			mem[write_addr+3] <= write_data[31:24] ; 
-			mem[write_addr+2] <= write_data[23:16] ; 
-			mem[write_addr+1] <= write_data[15:8] ; 
-			mem[write_addr]   <= write_data[7:0] ;
-		end 
-	end 	
+        // Port B — read 
+        .address_b      (word_addr_b),
+        .q_b		(readed_data),
+        .wren_b         (1'b0)
 
-endmodule 	
+        // unused
+      // .aclr0          (1'b0),
+      // .aclr1          (1'b0),
+      // .addressstall_a (1'b0),
+      // .addressstall_b (1'b0),
+      // .byteena_a      (1'b1), // no need to make the Instruction MEM byte-addressable
+      // .byteena_b      (1'b1), // no need to make the Instruction MEM byte-addressable
+      // .clocken0       (1'b1),
+      // .clocken1       (1'b1),
+      // .data_a         (32'b0),
+    );
+
+    defparam
+        altsyncram_component.operation_mode                     = "DUAL_PORT",
+        altsyncram_component.width_a                            = 32,
+        altsyncram_component.widthad_a                          = 12,
+        altsyncram_component.numwords_a                         = 4096,
+        altsyncram_component.width_b                            = 32,
+        altsyncram_component.widthad_b                          = 12,
+        altsyncram_component.numwords_b                         = 4096, // 16384 bytes / 4 = 4096 words 
+        altsyncram_component.outdata_reg_a                      = "CLOCK0",
+       // altsyncram_component.outdata_reg_b                      = "CLOCK0",
+        altsyncram_component.read_during_write_mode_mixed_ports = "OLD_DATA", //This shouldn't happen, it's software responsibility
+        altsyncram_component.intended_device_family             = "Cyclone V",
+        altsyncram_component.ram_block_type                     = "AUTO", 
+    	altsyncram_component.init_file 				= "table.hex" ; 
+
+endmodule
