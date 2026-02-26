5a5c01aaebfca7ee7ff4af996d86dcdc5a1222ef
Mohammed Taher <motaher20004@gmail.com>
2026-02-26 05:35:53 +0200

    Modify control_unit.v; include control logic for new added instructions

--- src/control_unit.v
+++ src/control_unit.v
@@ -20,7 +20,7 @@ module control_unit (
 			  branch = 1'b0 ;
 			  AluOp = 2'b10 ;
 		  end 
-		  7'b0000011 : begin // lw 
+		  7'b0000011 : begin // I-type  lw, lb, lh, lbu, lhu 
 			  Alusrc = 1'b1 ;
 			  Memtoreg = 1'b1 ;
 			  Regwrite = 1'b1 ;
@@ -29,7 +29,25 @@ module control_unit (
 			  branch = 1'b0 ;
 			  AluOp = 2'b00 ;
 		  end 
-		  7'b0100011 : begin //sw 
+		  7'b1100111 : begin // I-type jalr 
+			  Alusrc = 1'b1 ;
+			  Memtoreg = 1'b0 ;
+			  Regwrite = 1'b1 ;
+			  Memread = 1'b0 ;
+			  Memwrite = 1'b0 ;
+			  branch = 1'b1 ;
+			  AluOp = 2'b00 ;
+		  end 
+	 	  7'b0010011 : begin // I-type  addi, slli, xori, srli, srai, ori, andi.  
+			  Alusrc = 1'b1 ;
+			  Memtoreg = 1'b0 ;
+			  Regwrite = 1'b1 ;
+			  Memread = 1'b0 ;
+			  Memwrite = 1'b0 ;
+			  branch = 1'b0 ;
+			  AluOp = 2'b10 ;
+		  end	
+		  7'b0100011 : begin // S-type sw, sb, sh 
 			  Alusrc = 1'b1 ;
 			  Memtoreg = 1'b0 ; //don't care
 			  Regwrite = 1'b0 ;
@@ -38,7 +56,7 @@ module control_unit (
 			  branch = 1'b0 ;
 			  AluOp = 2'b00 ;
 		  end 
-		  7'b1100011 : begin //beq 
+		  7'b1100011 : begin // SB-type beq, bne, blt, bge, bltu, bgeu 
 			  Alusrc = 1'b0 ;
 			  Memtoreg = 1'b0 ; // don't care
 			  Regwrite = 1'b0 ;
@@ -47,7 +65,7 @@ module control_unit (
 			  branch = 1'b1 ;
 			  AluOp = 2'b01 ;
 		  end
-		 default {branch, Memread, Memtoreg, AluOp, Memwrite, Alusrc, Regwrite} ='b 0 ; 
+		 default : {branch, Memread, Memtoreg, AluOp, Memwrite, Alusrc, Regwrite} ='b 0 ; 
 
 
 	  endcase 
