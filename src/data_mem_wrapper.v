ffa24c8ba67da00081c1c41676bcf8a694fbc4d8
Mohammed Taher <motaher20004@gmail.com>
2026-04-26 18:00:33 +0300

    Modify th condition of byteena for masking to only act with stores

--- src/data_mem_wrapper.v
+++ src/data_mem_wrapper.v
@@ -14,30 +14,34 @@ module data_mem_wrapper (
     reg [3:0]  byteena;
 
 
-    wire [31:0] raw_out;
     always @(*) begin // to handle stores
-        case (funct3)
-            3'b000: begin  // sb
-                data_in = {4{w_data_MEM[7:0]}};
-                case (data_addr[1:0])
-                    2'b00: byteena = 4'b0001;
-                    2'b01: byteena = 4'b0010;
-                    2'b10: byteena = 4'b0100;
-                    2'b11: byteena = 4'b1000;
-                endcase
-            end
-            3'b001: begin  // sh
-                data_in = {2{w_data_MEM[15:0]}};
-                case (data_addr[1])
-                    1'b0: byteena = 4'b0011;
-                    1'b1: byteena = 4'b1100;
-                endcase
-            end
-            default: begin // sw
-                data_in = w_data_MEM;
-                byteena = 4'b1111;
-            end
-        endcase
+	    
+	    byteena = 4'b1111; data_in = w_data_MEM; 
+	    
+	    if (mem_wren) begin 
+        	case (funct3)
+        	    3'b000: begin  // sb
+        	        data_in = {4{w_data_MEM[7:0]}};
+        	        case (data_addr[1:0])
+        	            2'b00: byteena = 4'b0001;
+        	            2'b01: byteena = 4'b0010;
+        	            2'b10: byteena = 4'b0100;
+        	            2'b11: byteena = 4'b1000;
+        	        endcase
+        	    end
+        	    3'b001: begin  // sh
+        	        data_in = {2{w_data_MEM[15:0]}};
+        	        case (data_addr[1])
+        	            1'b0: byteena = 4'b0011;
+        	            1'b1: byteena = 4'b1100;
+        	        endcase
+        	    end
+        	    default: begin // sw
+        	        data_in = w_data_MEM;
+        	        byteena = 4'b1111;
+        	    end
+        	endcase
+	    end 
     end
 
 
