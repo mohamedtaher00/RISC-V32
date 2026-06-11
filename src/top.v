8b7a0dae2c6f8b34f54f3b12dc4f6df0cac9e927
Mohammed Taher <motaher20004@gmail.com>
2026-06-11 21:24:03 +0300

    Clean top.v

--- src/top.v
+++ src/top.v
@@ -57,8 +57,7 @@ module top (
 
 
 
-	// tricking (pipelined) 
-	output [31:0] instruction_test  	,	
+	    output [31:0] instruction_test  	,	
         output [31:0] write_back_data_test	, 
         output [9:0] pc_addr_test 		, 
         output [3:0] alu_ctrl_lines_test	,
@@ -70,23 +69,17 @@ module top (
 //        output [63:0] pc_64_addr_if  ,
 
 
-	output [1:0] alu_op_test 		, 
+    	output [1:0] alu_op_test 		, 
         output alu_src_test     		, 
         output branch_test      		, 
         output mem_write_ctrl_test  		,
         output reg_write_ctrl_test		,	
         output mem2reg_ctrl_test, 
 	
-	// memory interface 
-//	output [:] addr, // calculated at ex stage, then available at ex_mem  
-//	output [31:0] write_data, // sw -> register file provides the data at id stage, then available at id_ex  
-//	input [31:0] read_data, // lw -> data mem provides the data at mem stage, then available at mem_wb  
-//	output write_enable, // in case of sw, when do we need this to be on? simply when we write a periphral (mem stage) 
-//	output read_enable // in case of lw, when do we need this to be on? simply when we read a periphral (wb stage)
-	
+
 	// UART(tty) interface
-	input rx, 	
-	output tx
+	    input rx, 	
+	    output tx
 
 );
 
@@ -142,25 +135,25 @@ module top (
 	
 
 	reg [7:0] id_ex_ctrl_unit_current ;   
-        reg [63:0] id_ex_pc_current; 
-        reg [31:0] id_ex_immgen_current ;  
-        reg [4:0] id_ex_rs1_current   ;      
-        reg [4:0] id_ex_rs2_current   ;         
-        reg [4:0] id_ex_rd_current      ;
+    reg [63:0] id_ex_pc_current; 
+    reg [31:0] id_ex_immgen_current ;  
+    reg [4:0] id_ex_rs1_current   ;      
+    reg [4:0] id_ex_rs2_current   ;         
+    reg [4:0] id_ex_rd_current      ;
 	reg [3:0] id_ex_alu_ctrl_current ;
 	reg [63:0]id_ex_return_addr_current	;
-        reg [1:0] id_ex_our_prediction_current ; 	
+    reg [1:0] id_ex_our_prediction_current ; 	
 	reg [4:0] id_ex_previous_prediction_addr_current ; 	
 	
 	wire [7:0]  id_ex_ctrl_unit_nxt ;   
-        wire [63:0] id_ex_pc_nxt; 
-        wire [31:0] id_ex_immgen_nxt ;  
-        wire [4:0]  id_ex_rs1_nxt   ;      
-        wire [4:0]  id_ex_rs2_nxt   ;         
-        wire [4:0]  id_ex_rd_nxt      ;
+    wire [63:0] id_ex_pc_nxt; 
+    wire [31:0] id_ex_immgen_nxt ;  
+    wire [4:0]  id_ex_rs1_nxt   ;      
+    wire [4:0]  id_ex_rs2_nxt   ;         
+    wire [4:0]  id_ex_rd_nxt      ;
 	wire [3:0]  id_ex_alu_ctrl_nxt ;
 	wire [63:0] id_ex_return_addr_nxt	;
-        wire [1:0]  id_ex_our_prediction_nxt ; 	
+    wire [1:0]  id_ex_our_prediction_nxt ; 	
 	wire [4:0]  id_ex_previous_prediction_addr_nxt ; 
 	
 	
@@ -169,7 +162,7 @@ module top (
 	
 	wire stall_ctrl ;
 	wire stall_cnt ; 
-        wire [7:0] stall_mux_out ;	
+    wire [7:0] stall_mux_out ;	
 
 	
 	wire [31:0] reg_file_out1_id 	;		 
@@ -182,8 +175,8 @@ module top (
 	
 	// Alu/data path interface
 	reg [31:0] alu_src_1;
-        wire [31:0] alu_src_2 ;
-        reg [31:0]  alu_muxB_src ; 
+    wire [31:0] alu_src_2 ;
+    reg [31:0]  alu_muxB_src ; 
 	wire zero_flag ;
 	wire [31:0] alu_result_ex ;
 	wire [3:0] alu_sel ;
@@ -221,10 +214,10 @@ module top (
 	wire [4:0] previous_prediction_addr_mem ; 
 	
 	wire sel_imem_mem ;	 
-        wire sel_dmem_mem ;      
-        wire sel_uart_mem ;       
-        wire sel_gpio_mem ;	
-        wire sel_timer_mem; 	
+    wire sel_dmem_mem ;      
+    wire sel_uart_mem ;       
+    wire sel_gpio_mem ;	
+    wire sel_timer_mem; 	
 
 	wire [31:0] readed_data_uart ; 
 	
@@ -277,16 +270,7 @@ module top (
 		.addr_2_INST_MEM(pc_addr_if),
 		.addr_2_IF_ID_pipeline_reg(pc_64_addr_if)
 	);
-	// instruction memory 	
-//	instruction_mem #(.INST_MEMORY_SIZE(16384)) Instruction_MEM(
-//		.clk(clk),
-//		.stall(stall_cnt), 
-//		.read_addr(pc_addr_if), 
-//		.write_addr(ex_mem[101:70] ),// [101:70] alu_result 
-//		.write_data(ex_mem[133:102]), // write data
-//		.w_en(sel_imem_mem & ex_mem[2] ), // ex_mem[2] mem_write ctrl signal 
-//		.readed_data(instruction) 
-//	); 
+	
 	
 	// instr_mem_wrapper 
 	instr_mem_wrapper inst_mem(
@@ -322,7 +306,7 @@ module top (
 		.clk(clk), 
 		.addr_needs_predition(pc_addr_if[4:0]), //low order bits from the pc 
 		.previous_prediction_addr_ID_EX(id_ex [257:253]) ,//from ID/EX
-	        .previous_prediction_addr_MEM_WB(mem_wb[77:73]), 
+	    .previous_prediction_addr_MEM_WB(mem_wb[77:73]), 
 		.branch_EX_MEM(ex_mem[4]), 
 		.branch_MEM_WB(mem_wb[72]) , 
 		.final_verdict(ex_mem[145]) ,// 1 means taken, 0 means not taken -> from MEM/WB 
@@ -334,12 +318,12 @@ module top (
 	control_unit Control_Unit(
 		.instruction(instruction[6:0]),
 		.branch(branch_ctrl_id),
-                .Memread(mem_read_ctrl_id), 
-                .Memtoreg(memtoreg_ctrl_id), 
-                .AluOp(alu_op_ctrl_id), 
+        .Memread(mem_read_ctrl_id), 
+        .Memtoreg(memtoreg_ctrl_id), 
+        .AluOp(alu_op_ctrl_id), 
 		.Memwrite(mem_write_ctrl_id),
-	       	.Alusrc(alu_src_ctrl_id),
-	       	.Regwrite(reg_write_ctrl_id)
+	    .Alusrc(alu_src_ctrl_id),
+	    .Regwrite(reg_write_ctrl_id)
 	);
 
 	assign stall_mux_out = stall_ctrl ? 8'b0 :  {alu_op_ctrl_id, alu_src_ctrl_id, branch_ctrl_id,  mem_read_ctrl_id, mem_write_ctrl_id, reg_write_ctrl_id, memtoreg_ctrl_id} ;  
@@ -374,28 +358,28 @@ module top (
 	assign id_ex [182:178] = id_ex_rd_current      ;
 	assign id_ex [186:183] = id_ex_alu_ctrl_current ; // {funct 7 sixth bit, funct3}
 	assign id_ex [250:187] = id_ex_return_addr_current	;
-        assign id_ex [252:251] = id_ex_our_prediction_current ; 	
+    assign id_ex [252:251] = id_ex_our_prediction_current ; 	
 	assign id_ex [257:253] = id_ex_previous_prediction_addr_current ;
 
 
 	always @(posedge clk) begin //stuff that needed to be clocked  ; current state logic  
-		id_ex_alu_ctrl_current  <= id_ex_alu_ctrl_nxt    ;  
-		id_ex_ctrl_unit_current <= id_ex_ctrl_unit_nxt;
-		id_ex_pc_current        <= id_ex_pc_nxt ;   
-		id_ex_immgen_current    <= id_ex_immgen_nxt ; 
-		id_ex_rd_current	<= id_ex_rd_nxt ;  
-		id_ex_rs1_current	<= id_ex_rs1_nxt ; 
-		id_ex_rs2_current	<= id_ex_rs2_nxt ; 
-	        id_ex_return_addr_current <= id_ex_return_addr_nxt ; 
-		id_ex_our_prediction_current <= id_ex_our_prediction_nxt ; 
+		id_ex_alu_ctrl_current                 <= id_ex_alu_ctrl_nxt    ;  
+		id_ex_ctrl_unit_current                <= id_ex_ctrl_unit_nxt;
+		id_ex_pc_current                       <= id_ex_pc_nxt ;   
+		id_ex_immgen_current                   <= id_ex_immgen_nxt ; 
+		id_ex_rd_current	                   <= id_ex_rd_nxt ;  
+		id_ex_rs1_current	                   <= id_ex_rs1_nxt ; 
+		id_ex_rs2_current	                   <= id_ex_rs2_nxt ; 
+	    id_ex_return_addr_current              <= id_ex_return_addr_nxt ; 
+		id_ex_our_prediction_current           <= id_ex_our_prediction_nxt ; 
 		id_ex_previous_prediction_addr_current <= id_ex_previous_prediction_addr_nxt ;  
 	end
 
 	// next state logic 	
         assign id_ex_our_prediction_nxt = if_id [97:96] ; 	
-	assign id_ex_ctrl_unit_nxt = stall_mux_out 	; 
-	assign id_ex_alu_ctrl_nxt = {if_id[94], if_id[78:76]} ; // {funct7 sixth bit, funct3}
-	assign id_ex_pc_nxt =  if_id[63:0] ;
+    	assign id_ex_ctrl_unit_nxt = stall_mux_out 	; 
+    	assign id_ex_alu_ctrl_nxt = {if_id[94], if_id[78:76]} ; // {funct7 sixth bit, funct3}
+    	assign id_ex_pc_nxt =  if_id[63:0] ;
         assign id_ex_immgen_nxt = immgen_out_id ;                	
         assign id_ex_rd_nxt   = if_id[75:71] ; 
         assign id_ex_rs1_nxt  = if_id[83:79] ; 
@@ -453,7 +437,7 @@ module top (
 	assign ctrl_signals_ex = id_ex [4:0] ;
 	assign return_addr_ex = id_ex [250:187] ; 
 	assign previous_prediction_addr_ex_mem = id_ex [257:253] ;
-        assign funct3_ex = id_ex[185:183] ; 	
+    assign funct3_ex = id_ex[185:183] ; 	
 
 	always @(posedge clk) begin 
 		//current_state logic
@@ -465,14 +449,14 @@ module top (
 
 
 		ex_mem_current_state [68:5]     <= return_addr_ex ;
-		ex_mem_current_state [69]	   <= zero_flag 	     ; 
-		ex_mem_current_state [101:70]    <= alu_result_ex 	     ;
-		ex_mem_current_state [133:102]   <= write_data_ex     ;
-		ex_mem_current_state [138:134]   <= rd_ex 	     ;
-		ex_mem_current_state [139]	   <= branch_mispredicted_ex  ;
-		ex_mem_current_state [144:140]   <= previous_prediction_addr_ex_mem ;
-		ex_mem_current_state [145]	   <= final_verdict ;  
-        ex_mem_current_state [150:146] <= rs2_ex ; 
+		ex_mem_current_state [69]	    <= zero_flag 	     ; 
+		ex_mem_current_state [101:70]   <= alu_result_ex 	     ;
+		ex_mem_current_state [133:102]  <= write_data_ex     ;
+		ex_mem_current_state [138:134]  <= rd_ex 	     ;
+		ex_mem_current_state [139]	    <= branch_mispredicted_ex  ;
+		ex_mem_current_state [144:140]  <= previous_prediction_addr_ex_mem ;
+		ex_mem_current_state [145]	    <= final_verdict ;  
+        ex_mem_current_state [150:146]  <= rs2_ex ; 
 		funct3_ex_current <= funct3_ex 	; 
 	end
        // output logic 
@@ -523,7 +507,7 @@ module top (
 	// forwarding unit
 	 
 	forwarding_unit Forwarding_Unit(
-		.ID_EX_RegisterRs1(id_ex[172:168]),
+	    	.ID_EX_RegisterRs1(id_ex[172:168]),
 	        .ID_EX_RegisterRs2(id_ex[177:173]),
 	                           
 	        .EX_MEM_RegisterRd(ex_mem[138:134]),
@@ -532,8 +516,8 @@ module top (
 	        .EX_MEM_RegWrite(ex_mem[1]),
 	        .MEM_WB_RegWrite(mem_wb[1]),
 
-		.ForwardA(forwardA), 
-                .ForwardB(forwardB)
+		    .ForwardA(forwardA), 
+            .ForwardB(forwardB)
 	);
 	//forwarding muxs check out FIGURE 4.57 @ the book
 	always @(*) begin
@@ -573,8 +557,8 @@ module top (
 						mem_wb_current_state [39]  , 	
 						mem_wb_current_state [38:34], 
 						mem_wb_current_state [33:2], 
-                                                readed_data_mem, // doesn't need to be clocked, already clocked from the data_memory
-                                                mem_wb_current_state [1:0]
+                        readed_data_mem, // doesn't need to be clocked, already clocked from the data_memory
+                        mem_wb_current_state [1:0]
 	} ; 
 
 	//next state logic 
@@ -590,7 +574,7 @@ module top (
 	always @(posedge clk) begin
 	       // current_state_logic	
 		mem_wb_current_state [1:0]   <= ctrl_signals_mem ;	
-		mem_wb_current_state [33:2] <= alu_result_mem ; 
+		mem_wb_current_state [33:2]  <= alu_result_mem ; 
 		mem_wb_current_state [38:34] <= rd_mem		;
 		mem_wb_current_state [39]    <= branch_mispredicted_mem ; 
 		mem_wb_current_state [40]    <= branch_mem 	; 
@@ -598,14 +582,7 @@ module top (
 	    mem_wb_current_state [48:46] <= funct3_mem ; 	
 	end 
 
-//	data_mem # (.MEMORY_SIZE(12288)) Data_MEM(
-//	.clk(clk),
-//	.addr(alu_result_mem[$clog2(2048)-1:0]),
-//	.we(ex_mem[2] & sel_dmem_mem), //memory write ctrl signal 
-//	.re(ex_mem[3]), //memory read ctrl signal  it's no effect on the data_mem really, but maybe the logic appeaers in the future and we add it (i predict the nop)	
-//	.w_data_MEM(ex_mem [133:102]),
-//	.data(readed_data_mem_mem) 
-//	);
+
     
     assign write_data_mem = ((ex_mem[153:149] == mem_wb[70:66]) & mem_wb[1]) ? mem_wb[65:34] : ex_mem[133:102] ; // ex_mem[153:149] is rs2 
                                                                                                                 // mem_wb[70:66] is rd  
@@ -640,12 +617,12 @@ module top (
 	// UART
 	uart uart_wrapper(
 		.clk(clk) ,             	
-                .write_data(ex_mem [133:102]), 
-                .data_in_rx(rx), 
+        .write_data(ex_mem [133:102]), 
+        .data_in_rx(rx), 
 		.data_out_tx(tx) , 
-                .mem_write(ex_mem[2]) , 
-                .sel_uart(sel_uart_mem) , 
-                .addr(alu_result_mem[3:0]), 
+        .mem_write(ex_mem[2]) , 
+        .sel_uart(sel_uart_mem) , 
+        .addr(alu_result_mem[3:0]), 
 		.readed_data(readed_data_uart) 
 	); 	
 
