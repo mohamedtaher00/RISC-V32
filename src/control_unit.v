0de10051442de4be3f72de8df9edae63e8a07e3e
Mohammed Taher <motaher20004@gmail.com>
2026-06-17 17:19:51 +0300

    Include jal, jalr in control_unit.v

--- src/control_unit.v
+++ src/control_unit.v
@@ -2,10 +2,11 @@ module control_unit (
 	input [6:0] instruction, 
 
 	output reg branch,
-       	output reg Memread, 
+    output reg Memread, 
 	output reg Memtoreg, 
 	output reg [1:0] AluOp, 
-	output reg Memwrite, Alusrc, Regwrite
+	output reg Memwrite, Alusrc, Regwrite, 
+    output reg is_jalr
 );
   
   always @(*) begin
@@ -19,6 +20,7 @@ module control_unit (
 			  Memwrite = 1'b0 ;
 			  branch = 1'b0 ;
 			  AluOp = 2'b10 ;
+              is_jalr = 1'b0 ;
 		  end 
 		  7'b0000011 : begin // I-type  lw, lb, lh, lbu, lhu 
 			  Alusrc = 1'b1 ;
@@ -28,6 +30,7 @@ module control_unit (
 			  Memwrite = 1'b0 ;
 			  branch = 1'b0 ;
 			  AluOp = 2'b00 ;
+              is_jalr = 1'b0 ;
 		  end 
 		  7'b1100111 : begin // I-type jalr 
 			  Alusrc = 1'b1 ;
@@ -37,6 +40,7 @@ module control_unit (
 			  Memwrite = 1'b0 ;
 			  branch = 1'b1 ;
 			  AluOp = 2'b00 ;
+              is_jalr = 1'b1 ;
 		  end 
 	 	  7'b0010011 : begin // I-type  addi, slli, xori, srli, srai, ori, andi.  
 			  Alusrc = 1'b1 ;
@@ -46,6 +50,7 @@ module control_unit (
 			  Memwrite = 1'b0 ;
 			  branch = 1'b0 ;
 			  AluOp = 2'b11 ;
+              is_jalr = 1'b0 ;
 		  end	
 		  7'b0100011 : begin // S-type sw, sb, sh 
 			  Alusrc = 1'b1 ;
@@ -55,6 +60,7 @@ module control_unit (
 			  Memwrite = 1'b1 ;
 			  branch = 1'b0 ;
 			  AluOp = 2'b00 ;
+              is_jalr = 1'b0 ;
 		  end 
 		  7'b1100011 : begin // SB-type beq, bne, blt, bge, bltu, bgeu 
 			  Alusrc = 1'b0 ;
@@ -64,8 +70,19 @@ module control_unit (
 			  Memwrite = 1'b0 ;
 			  branch = 1'b1 ;
 			  AluOp = 2'b01 ;
+              is_jalr = 1'b0 ;
 		  end
-		 default : {branch, Memread, Memtoreg, AluOp, Memwrite, Alusrc, Regwrite} ='b 0 ; 
+          7'b1101111 : begin // jal, J-type 
+              Alusrc = 1'b1 ;// don't care; the additoin would be done by separate adder not the ALU 
+              Memtoreg = 1'b0 ;
+              Regwrite = 1'b1 ;
+              Memread = 1'b0 ;
+              Memwrite = 1'b0 ;
+              branch = 1'b1 ;
+              AluOp = 2'b00 ;
+              is_jalr = 1'b0 ;
+          end 
+		 default : {branch, Memread, Memtoreg, AluOp, Memwrite, Alusrc, Regwrite, is_jalr} ='b 0 ; 
 
 
 	  endcase 
