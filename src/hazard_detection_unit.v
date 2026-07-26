871ef575a88ffe929f7205eb12225d14c1c47d16
Mohammed Taher <motaher20004@gmail.com>
2026-07-26 22:42:38 +0300

    Include jal&jalr in the flush logic

--- src/hazard_detection_unit.v
+++ src/hazard_detection_unit.v
@@ -1,37 +1,47 @@
 module hazard_detection_unit (
-	input ID_EX_MemRead, 
-	input [4:0] ID_EX_RegisterRd, 
-	input [4:0] IF_ID_Rs1, 
-	input [4:0] IF_ID_Rs2,  
-	input [1:0] boot_cnt, 
-	input MEM_WB_we_were_wrong, 
+	input ID_EX_MemRead,
+	input [4:0] ID_EX_RegisterRd,
+	input [4:0] IF_ID_Rs1,
+	input [4:0] IF_ID_Rs2,
+	input [1:0] boot_cnt,
+	input MEM_WB_we_were_wrong,
 	input MEM_WB_branch,
-        input our_prediction_ID_EX, 
-	input branch_ID_EX, 
-	input we_were_wrong_EX_MEM, 	
-	output reg stall_ctrl,
-        output reg stall_cnt	
+    input our_prediction_ID_EX,
+	input branch_id_ex,
+	input we_were_wrong_EX_MEM,
+
+    input reg_w_id_ex,
+    input is_jalr_id_ex,
+    input branch_ex_mem,
+    input reg_w_ex_mem,
+    input is_jalr_ex_mem,
+
+	output reg flush,
+    output reg stall_cnt
 );
 	always @(*) begin
-	  stall_ctrl = 1'b0 ; 
+	  flush = 1'b0 ;
 	  if (boot_cnt <= 2'b01)
-	  	  stall_ctrl = 1'b0 ; 	  
-	  else if (((ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) ) ) | (MEM_WB_we_were_wrong & MEM_WB_branch) | (our_prediction_ID_EX & branch_ID_EX))  
-		  stall_ctrl = 1'b1 ; 
-	  else 
-		  stall_ctrl = 1'b0 ; 
+	  	  flush = 1'b0 ;
+	  else if (((ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) ) ) // lw-use
+          | ((MEM_WB_we_were_wrong & MEM_WB_branch) | (our_prediction_ID_EX & branch_id_ex)) // prev. misprediction or current prediction
+          | (is_jalr_ex_mem | is_jalr_id_ex) // jalr has penalty of 2 clock-cycles
+          | (branch_id_ex & reg_w_id_ex & ~is_jalr_id_ex)) // jal has penalty of 1 clock-cycle
+		  flush = 1'b1 ;
+	  else
+		  flush = 1'b0 ;
 	end
 
-	always @(*) begin 
-		stall_cnt = 1'b0 ; 
-		if (boot_cnt <= 2'b01) 
-			stall_cnt = 1'b0 ; 
-		else if ((~we_were_wrong_EX_MEM) && (ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) )) 
-			stall_cnt = 1'b1 ; 
-		else 
-			stall_cnt = 1'b0 ; 
+	always @(*) begin
+		stall_cnt = 1'b0 ;
+		if (boot_cnt <= 2'b01)
+			stall_cnt = 1'b0 ;
+		else if ((~we_were_wrong_EX_MEM) && (ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) ))
+			stall_cnt = 1'b1 ;
+		else
+			stall_cnt = 1'b0 ;
 
-	end 	
+	end
 
 
-endmodule 
+endmodule
