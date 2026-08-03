49ef0708e3f4784fcc6a8027ce9f4337390f2bd9
Mohammed Taher <motaher20004@gmail.com>
2026-08-03 14:57:05 +0300

    Extract lw_use signal, exclude lui/auipc/jal from load-use hazard
    Factored the repeated lw-use condition (ID_EX_MemRead + Rd/Rs1/Rs2
    match) into a single `lw_use` wire for readability.
    
    lui/auipc/jal don't actually read rs1/rs2 in EX, but their
    instruction-field bits at the rs1/rs2 position can coincidentally
    match a prior Rd, causing spurious load-use hazards. Gated lw_use
    with ~is_lui, ~is_auipc, ~is_jal to eliminate these false stalls.

--- src/hazard_detection_unit.v
+++ src/hazard_detection_unit.v
@@ -9,6 +9,9 @@ module hazard_detection_unit (
     input our_prediction_ID_EX,
 	input branch_id_ex,
 	input we_were_wrong_EX_MEM,
+    input is_lui,
+    input is_auipc,
+    input is_jal,
 
     input reg_w_id_ex,
     input is_jalr_id_ex,
@@ -19,11 +22,15 @@ module hazard_detection_unit (
 	output reg flush,
     output reg stall_cnt
 );
+    wire lw_use;
+    assign lw_use = ((ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) ) //lw-use
+        & (~is_lui) & (~is_auipc) &(~is_jal)); //stuff)
+
 	always @(*) begin
 	  flush = 1'b0 ;
 	  if (boot_cnt <= 2'b01)
 	  	  flush = 1'b0 ;
-	  else if (((ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) ) ) // lw-use
+	  else if (lw_use
           | ((MEM_WB_we_were_wrong & MEM_WB_branch) | (our_prediction_ID_EX & branch_id_ex)) // prev. misprediction or current prediction
           | (is_jalr_ex_mem | is_jalr_id_ex) // jalr has penalty of 2 clock-cycles
           | (branch_id_ex & reg_w_id_ex & ~is_jalr_id_ex)) // jal has penalty of 1 clock-cycle
@@ -36,7 +43,7 @@ module hazard_detection_unit (
 		stall_cnt = 1'b0 ;
 		if (boot_cnt <= 2'b01)
 			stall_cnt = 1'b0 ;
-		else if ((~we_were_wrong_EX_MEM) && (ID_EX_MemRead) && ((ID_EX_RegisterRd[4:0] == IF_ID_Rs1[4:0]) || (ID_EX_RegisterRd[4:0] == IF_ID_Rs2[4:0]) ))
+		else if ((~we_were_wrong_EX_MEM) && lw_use)
 			stall_cnt = 1'b1 ;
 		else
 			stall_cnt = 1'b0 ;
