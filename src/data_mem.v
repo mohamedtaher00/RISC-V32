a1244be9686f2cc0f39d54bb2da614d40b0a7c30
Mohammed Taher <motaher20004@gmail.com>
2026-08-19 20:51:54 +0300

    data_mem: fix device family, simplify read-during-write mode, cleanup
    - intended_device_family: Cyclone V -> Cyclone IV E (matches actual target)
    - read_during_write_mode_port_a: NEW_DATA_NO_NBE_READ -> DONT_CARE
    - strip trailing whitespace, drop stale byte_addr/UNREGISTERED comments

--- src/data_mem.v
+++ src/data_mem.v
@@ -7,17 +7,17 @@ module data_mem
 		input re,
 		input [31:0] w_data_MEM, // data_in
 		input clk,
-		
+
 
 
 		// Embedded memory configuration signals
-//		input [13:0] byte_addr, 
+//		input [13:0] byte_addr,
                 input [3:0]  byteena,
 
-		output [31:0] data // data_out  
-	); 
+		output [31:0] data // data_out
+	);
 
-      
+
 	wire [11:0] word_addr = addr[13:2];
 
 	altsyncram altsyncram_component (
@@ -26,7 +26,7 @@ module data_mem
        		 .data_a         (w_data_MEM),
        		 .wren_a         (we),
        		 .byteena_a      (byteena),
-       		 .q_a            (data), 
+       		 .q_a            (data),
 
        		 // unused
        		 .aclr0          (1'b0),
@@ -41,12 +41,12 @@ module data_mem
         altsyncram_component.operation_mode                = "SINGLE_PORT",
         altsyncram_component.width_a                       = 32,
         altsyncram_component.widthad_a                     = 12,
-        altsyncram_component.numwords_a                    = 3072 , // 12288 bytes / 4 = 3072 
+        altsyncram_component.numwords_a                    = 3072 , // 12288 bytes / 4 = 3072
         altsyncram_component.width_byteena_a               = 4,
         altsyncram_component.byte_size                     = 8,
-        altsyncram_component.outdata_reg_a                 = "UNREGISTERED", // it was CLOCK0
-        altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ", // don't care really (it'll never gonna happen) 
-        altsyncram_component.intended_device_family        = "Cyclone V",
+        altsyncram_component.outdata_reg_a                 = "UNREGISTERED",
+        altsyncram_component.read_during_write_mode_port_a = "DONT_CARE", // don't care really (it'll never gonna happen)
+        altsyncram_component.intended_device_family        = "Cyclone IV E",
         altsyncram_component.ram_block_type                = "AUTO";
 
 
